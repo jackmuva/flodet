30fdda249b705836c5534b6278db7d9ea833aa0d
Jack Mu <jackmu@umich.edu>
2025-02-09 13:17:37 -0600

    fixed an overlap issue when the message panel got too big

--- src/app/components/log/LogMessagePanel.tsx
+++ src/app/components/log/LogMessagePanel.tsx
@@ -2,7 +2,7 @@
 export const LogMessagePanel = (panelProps: { messages: Array<any> }) => {
 	return (
 		<div className="w-full flex flex-col items-center">
-			<div className="flex w-full md:w-1/2 items-stretch space-x-1 h-10">
+			<div className="flex w-full md:w-1/2 items-stretch space-x-1 min-h-10 h-fit">
 				<div className="bg-slate-900 font-semibold text-green-300 p-2 rounded-lg border-2 w-5/6 h-fit">
 					{panelProps.messages.map((message) => {
 						return (
